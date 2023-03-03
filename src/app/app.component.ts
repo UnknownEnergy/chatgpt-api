9c01a8b19aa7855126a25c8f87a8041f2112d036
daniel <daniel06061@gmail.com>
2023-03-03 21:17:01 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Configuration, OpenAIApi} from "openai";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chatgpt-api';
+
+  messageInput = '';
+  messages: { content: string; timestamp: Date; avatar: string; isUser: boolean; }[] = [];
+  chatbotTyping = false;
+  apikey = '';
+  chatHistory = [];
+  usedTokens: number = 0;
+
+  @ViewChild('messageContainer', { static: false }) messageContainer: ElementRef;
+
+  constructor() { }
+
+  async sendMessage() {
+    this.chatHistory.push(this.messageInput);
+    const configuration = new Configuration({
+      apiKey: this.apikey,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    // Add the user's message to the chat
+    this.messages.push({
+      content: this.messageInput,
+      timestamp: new Date(),
+      avatar: '<i class="bi bi-person-circle"></i>',
+      isUser: true
+    });
+
+    // Clear the message input field
+    this.messageInput = '';
+
+    // Set the chatbot typing indicator to true
+    this.chatbotTyping = true;
+
+    setTimeout(() => {
+      this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+    }, 0);
+
+    // Generate a response from the chatbot
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: this.chatHistory.join('\n') + '\n' + this.messages[this.messages.length - 1].content,
+      temperature: 0,
+      max_tokens: 100,
+    });
+
+    // Add the chatbot's response to the chat
+    if (response && response.data && response.data.choices && response.data.choices.length > 0) {
+      const message = response.data.choices[0].text;
+      this.messages.push({
+        content: message,
+        timestamp: new Date(),
+        avatar: '<i class="bi bi-laptop"></i>',
+        isUser: false,
+      });
+      this.chatHistory.push(message);
+    }
+
+    // Set the chatbot typing indicator to false
+    this.chatbotTyping = false;
+
+    setTimeout(() => {
+      this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+    }, 0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
