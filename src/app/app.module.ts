e3718d3d491eb97044b9d5e0de7b605cd589173e
daniel <daniel06061@gmail.com>
2023-03-22 16:00:15 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { UsageComponent } from './toolbar/usage/usage.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SettingsComponent } from './toolbar/settings/settings.component';
 import { ChatContainerComponent } from './chat-container/chat-container.component';
-import { ApiStatusComponent } from './toolbar/api-status/api-status.component';
+import { ApiStatusComponent } from './header/api-status/api-status.component';
 
 const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt();
 
